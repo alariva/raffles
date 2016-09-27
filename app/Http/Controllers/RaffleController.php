dc8b0e0a361daf4a1eb1f791c7f49d552d7cad58
Ariel Vallese <alariva@gmail.com>
2016-09-27 17:18:40 -0300

    Init cart session

--- app/Http/Controllers/RaffleController.php
+++ app/Http/Controllers/RaffleController.php
@@ -2,14 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\CORE\RaffleDealer;
-use App\Coupon;
 use App\Raffle;
 
 class RaffleController extends Controller
 {
     public function home(Raffle $raffle)
     {
+        session()->put('cart.raffle', $raffle);
         session()->put('cart.numbers', []);
 
         return view('raffles.home', compact('raffle'));
