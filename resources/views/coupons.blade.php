a60578b39a5eb87696a3610dceaa6ae3eea215e6
Ariel Vallese <alariva@gmail.com>
2016-09-26 23:58:29 -0300

    Add SB Admin theme

--- resources/views/coupons.blade.php
+++ resources/views/coupons.blade.php
@@ -1,3 +1,22 @@
-@foreach($coupons as $coupon)
-    {{ $coupon }}<br/>
-@endforeach
+@extends('layouts.public')
+@section('page_heading','Coupons')
+@section('section')
+
+<div class="col-xs-6 col-md-3">
+    <div class="panel panel-default">
+      <!-- Default panel contents -->
+      <div class="panel-heading">Rifas</div>
+      <div class="panel-body">
+        <p>Elegí tu número</p>
+      </div>
+
+      <!-- List group -->
+      <ul class="list-group">
+        @foreach($coupons as $coupon)
+            <a href="#"><button type="button" class="list-group-item">Numero {{ $coupon }} </button></a>
+        @endforeach
+      </ul>
+    </div>
+</div>
+
+@stop
---
+++ resources/views/layouts/public.blade.php
@@ -0,0 +1,101 @@
+@extends('layouts.plane')
+
+@section('body')
+ <div id="wrapper">
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="{{ url ('') }}">Rifas</a>
+            </div>
+            <!-- /.navbar-header -->
+
+            <ul class="nav navbar-top-links navbar-right">
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                        <i class="fa fa-envelope fa-fw"></i>  <i class="fa fa-caret-down"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-messages">
+                        <li>
+                            <a href="#">
+                                <div>
+                                    <strong>John Smith</strong>
+                                    <span class="pull-right text-muted">
+                                        <em>Yesterday</em>
+                                    </span>
+                                </div>
+                                <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...</div>
+                            </a>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <a href="#">
+                                <div>
+                                    <strong>John Smith</strong>
+                                    <span class="pull-right text-muted">
+                                        <em>Yesterday</em>
+                                    </span>
+                                </div>
+                                <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...</div>
+                            </a>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <a class="text-center" href="#">
+                                <strong>Read All Messages</strong>
+                                <i class="fa fa-angle-right"></i>
+                            </a>
+                        </li>
+                    </ul>
+                    <!-- /.dropdown-messages -->
+                </li>
+                <!-- /.dropdown -->
+            </ul>
+            <!-- /.navbar-top-links -->
+
+            <div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+                        <li class="sidebar-search">
+                            <div class="input-group custom-search-form">
+                                <input type="text" class="form-control" placeholder="Search...">
+                                <span class="input-group-btn">
+                                <button class="btn btn-default" type="button">
+                                    <i class="fa fa-search"></i>
+                                </button>
+                            </span>
+                            </div>
+                            <!-- /input-group -->
+                        </li>
+                        <li {{ (Request::is('/') ? 'class="active"' : '') }}>
+                            <a href="{{ url ('') }}"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.sidebar-collapse -->
+            </div>
+            <!-- /.navbar-static-side -->
+        </nav>
+
+        <div id="page-wrapper">
+             <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">@yield('page_heading')</h1>
+                </div>
+                <!-- /.col-lg-12 -->
+           </div>
+            <div class="row">  
+                @yield('section')
+
+            </div>
+            <!-- /#page-wrapper -->
+        </div>
+    </div>
+@stop
+
