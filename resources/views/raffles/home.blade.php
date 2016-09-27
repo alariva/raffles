0dbc98f0dc5c8feed80b156a580a8c9ef64cac66
Ariel Vallese <alariva@gmail.com>
2016-09-27 16:56:28 -0300

    Update layout

--- resources/views/raffles/home.blade.php
+++ resources/views/raffles/home.blade.php
@@ -2,8 +2,10 @@
 @section('page_heading', $raffle->name)
 @section('section')
 
-<div class="container">           
+<div class="container-fluid">     
     {!! Markdown::convertToHtml( $raffle->description ) !!}
+
+    {!! Button::primary('Elegi tus numeros de la suerte acá')->block()->large() !!}
 </div>
 
 @stop
