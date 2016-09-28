97c4eece671cddefb2627405ec7a99b9d5061b31
Ariel Vallese <alariva@gmail.com>
2016-09-28 14:57:56 -0300

    Enhance layout sidebar

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -62,20 +62,16 @@
             <div class="navbar-default sidebar" role="navigation">
                 <div class="sidebar-nav navbar-collapse">
                     <ul class="nav" id="side-menu">
-                        <li class="sidebar-search">
-                            <div class="input-group custom-search-form">
-                                <input type="text" class="form-control" placeholder="Search...">
-                                <span class="input-group-btn">
-                                <button class="btn btn-default" type="button">
-                                    <i class="fa fa-search"></i>
-                                </button>
-                            </span>
-                            </div>
-                            <!-- /input-group -->
-                        </li>
+                    @if($raffle = session('cart.raffle'))
                         <li {{ (Request::is('/') ? 'class="active"' : '') }}>
-                            <a href="{{ url ('') }}"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+                            <a href="{{ route('raffle.home', $raffle)}}"><i class="fa fa-undo fa-fw"></i> Recomenzar</a>
                         </li>
+                    @endif
+                    @if($numbers = session('cart.numbers'))
+                        <li {{ (Request::is('/') ? 'class="active"' : '') }}>
+                            <a href="#"><i class="fa fa-tags fa-fw"></i>{{ count($numbers) }} Números</a>
+                        </li>
+                    @endif
                     </ul>
                 </div>
                 <!-- /.sidebar-collapse -->
