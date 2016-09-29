6a55f681dcd3830c96b44f8df2459d361c6d6039
Ariel Vallese <alariva@gmail.com>
2016-09-29 00:37:29 -0300

    Add closing date alerts to home page

--- resources/views/raffles/home.blade.php
+++ resources/views/raffles/home.blade.php
@@ -2,13 +2,23 @@
 @section('page_heading', $raffle->name)
 @section('section')
 
-<div class="container-fluid">     
+<div class="container-fluid">
+
     {!! Markdown::convertToHtml( $raffle->description ) !!}
+
+    @if($raffle->closed_at->isFuture())
+    {!! Alert::info('La entrega de talones cierra '.$raffle->closed_at->diffForHumans()) !!}
 
     {!! Button::primary('Elegi tus numeros de la suerte acá')
                 ->block()
                 ->large()
                 ->asLinkTo(route('coupons.browse', $raffle)) !!}
+    @else
+    {!! Alert::warning('La entrega de talones cerró '.$raffle->closed_at->diffForHumans().'! Mucha suerte!') !!}
+    @endif
+
 </div>
+
+<br/>
 
 @stop
