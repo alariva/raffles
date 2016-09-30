cc74a2d58272a5cb66b5a892b862e564f32cb786
Ariel Vallese <alariva@gmail.com>
2016-09-30 00:31:10 -0300

    Add view composer for raffle sharing

---
+++ app/Providers/ComposerServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+
+class ComposerServiceProvider extends ServiceProvider
+{
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Using Closure based composers...
+        View::composer('layouts.public', function ($view) {
+            $view->with('raffle', session('cart.raffle'));
+        });
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
