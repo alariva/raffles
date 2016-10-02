68f2bd3e0cba5067dcd6505bfb2e93fe42f033b9
Ariel Vallese <alariva@gmail.com>
2016-10-02 20:58:54 -0300

    Fix coupon browsing responsive layout

--- resources/views/coupons.blade.php
+++ resources/views/coupons.blade.php
@@ -3,7 +3,7 @@
 @section('section')
 
 <div class="container-fluid">
-<div class="col-xs-8 col-md-8 col-md-offset-2 col-sm-12">
+<div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-12 col-xs-12">
     <div class="panel panel-default">
       <!-- Default panel contents -->
       <div class="panel-heading">Quedan {{ $count }} talones disponibles</div>
@@ -14,7 +14,7 @@
 
           <div class="row">
           @foreach($chunk as $coupon)
-          <div class="col-sm-3">
+          <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
             @if(in_array($coupon, $selected))
                 <button type="button" class="list-group-item list-group-item-info" disabled>
                   <big><i class="fa fa-tag"></i>&nbsp;N°{{ $coupon }}</big><i class="fa fa-check"></i>
