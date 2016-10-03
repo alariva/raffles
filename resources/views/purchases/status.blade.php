ae7eda57b07a5457631c6110f16e6b5d764ef8e5
Ariel Vallese <alariva@gmail.com>
2016-10-02 21:02:35 -0300

    Fix purchase status responsive layout

--- resources/views/purchases/status.blade.php
+++ resources/views/purchases/status.blade.php
@@ -3,7 +3,7 @@
 @section('section')
 
 <div class="container-fluid">
-<div class="col-xs-8 col-md-8 col-md-offset-2 col-sm-12">
+<div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-12 col-xs-12">
     <div class="panel panel-default">
       <!-- Default panel contents -->
       <div class="panel-heading">Estado de tu reserva</div>
